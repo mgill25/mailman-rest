faf262c8d5ec320b0a48055e619f218f95e9ef75
Manish Gill <gill.manish90@gmail.com>
2013-06-19 18:26:30 +0530

    Base model; Core and local mix-ins for abstract list

--- website/public_rest/models.py
+++ website/public_rest/models.py
@@ -3,18 +3,24 @@ from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
 
 
+class BaseModel(models.Model):
+    class Meta:
+        abstract = True
+
+
 class ListManager(models.Manager):
-    def create_list(self, list_name, mail_host, fqdn_listname):
+    def create_list(self, list_name, mail_host, fqdn_listname, **extra_fields):
         if not list_name or not fqdn_listname or not mail_host:
             raise ValueError("Invalid or Absent parameter.")
         now = timezone.now()
+        print("Creating a List")
         lst = self.model(created_at=now, list_name=list_name, mail_host=mail_host,
                 fqdn_listname=fqdn_listname, **extra_fields)
         lst.save()
         return lst
 
 
-class AbstractBaseList(models.Model):
+class AbstractBaseList(BaseModel):
     created_at = models.DateTimeField(default=timezone.now())
 
     class Meta:
@@ -24,12 +30,25 @@ class AbstractBaseList(models.Model):
         return self.list_name
 
 
-class AbstractMailingList(AbstractBaseList):
+class CoreListMixin(models.Model):
+    """Fields that are part of the Mailman core."""
     list_name = models.CharField(max_length=100)        # TODO: Verify properties like max-length from MM-core
     mail_host = models.CharField(max_length=100,
             help_text=_("Domain Name hosting this mailing list"))
     fqdn_listname = models.CharField(max_length=100,
             help_text=_("Fully qualified name of the list. It is comprised of list_name + '@' + mail_host"))
+
+    class Meta:
+        abstract = True
+
+
+class LocalListMixin(models.Model):
+    """Fields that are added by us, locally."""
+    class Meta:
+        abstract = True
+
+
+class AbstractMailingList(AbstractBaseList, CoreListMixin, LocalListMixin):
 
     objects = ListManager()
 
