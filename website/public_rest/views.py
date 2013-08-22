c66019dded29807aa24596dc2d398276a86133b0
Manish Gill <gill.manish90@gmail.com>
2013-08-22 20:07:00 +0530

    Verification for users is not being filtered properly

--- website/public_rest/views.py
+++ website/public_rest/views.py
@@ -12,7 +12,11 @@ from .models import User, Membership, MailingList, Domain, Email
 #logging
 logger = logging.getLogger(__name__)
 
-class UserViewSet(viewsets.ModelViewSet):
+class BaseModelViewSet(viewsets.ModelViewSet):
+    def str2bool(self, s):
+        return s.lower() in ['True']
+
+class UserViewSet(BaseModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
     """
@@ -21,7 +25,6 @@ class UserViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         queryset = User.objects.all()
-        logger.debug("QUERY_PARAMS: {0}".format(self.request.QUERY_PARAMS))
         display_name = self.request.QUERY_PARAMS.get('display_name', None)
         email = self.request.QUERY_PARAMS.get('email', None)
         # now filter out query params
@@ -29,36 +32,36 @@ class UserViewSet(viewsets.ModelViewSet):
             queryset = queryset.filter(display_name=display_name)
         if email is not None:
             queryset = queryset.filter(email__address=email)
-            verified = self.request.QUERY_PARAMS.get('verified', None)
-            if verified is not None:
-                queryset = queryset.filter(email__verified=verified)
         return queryset
 
 
-class EmailViewSet(viewsets.ModelViewSet):
+class EmailViewSet(BaseModelViewSet):
     queryset = Email.objects.all()
     serializer_class = EmailSerializer
     filter_fields = ('user', 'address', 'verified',)
 
     def get_queryset(self):
         queryset = Email.objects.all()
-        logger.debug("QUERY_PARAMS: {0}".format(self.request.QUERY_PARAMS))
         address = self.request.QUERY_PARAMS.get('address', None)
         user = self.request.QUERY_PARAMS.get('user', None)
+        verified = self.request.QUERY_PARAMS.get('verified', None)
         if address is not None:
             queryset = queryset.filter(address=address)
+        if verified is not None:
+            verified = self.str2bool(verified)
+            queryset = queryset.filter(verified=verified)
         if user is not None:
             queryset = queryset.filter(user__display_name=user)
         return queryset
 
 
-class MembershipViewSet(viewsets.ModelViewSet):
+class MembershipViewSet(BaseModelViewSet):
 
     queryset = Membership.objects.all()
     serializer_class = MembershipSerializer
 
 
-class MailingListViewSet(viewsets.ModelViewSet):
+class MailingListViewSet(BaseModelViewSet):
 
     queryset = MailingList.objects.all()
     serializer_class = MailingListSerializer
@@ -77,7 +80,7 @@ class MailingListViewSet(viewsets.ModelViewSet):
         return response.Response(serializer.data)
 
 
-class DomainViewSet(viewsets.ModelViewSet):
+class DomainViewSet(BaseModelViewSet):
     queryset = Domain.objects.all()
     serializer_class = DomainSerializer
 
