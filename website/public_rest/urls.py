d1adc283691818f27493f11fd0d4644b4112b771
Manish Gill <gill.manish90@gmail.com>
2013-09-11 22:01:04 +0530

    Reformatting urls.py

--- website/public_rest/urls.py
+++ website/public_rest/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
-#router.register(r'memberships', views.MembershipViewSet)
 router.register(r'domains', views.DomainViewSet)
 router.register(r'lists', views.MailingListViewSet)
 router.register(r'emails', views.EmailViewSet)
@@ -23,13 +22,13 @@ membershipprefs_detail = views.MembershipPrefsViewSet.as_view(detail_dict)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browseable API.
 urlpatterns = patterns('',
+    url(r'^api/', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^api/lists/(?P<pk>[^/]+)/settings/$', listsettings_detail, name='listsettings-detail'),
     url(r'^api/lists/(?P<pk>[^/]+)/settings/\.(?P<format>[a-z]+)$', listsettings_detail),
     url(r'^api/lists/(?P<list_id>[^/]+)/(?P<role>members|owners|moderators)/(?P<address>[^/]+)/$',
         membership_detail, name='membership-detail'),
-    url(r'^api/', include(router.urls)),
-    url(r'^api/emails/(?P<pk>[^/]+)/preferences/$', emailprefs_detail, name='emailprefs-detail'),
     url(r'^api/lists/(?P<list_id>[^/]+)/(?P<role>members|owners|moderators)/(?P<address>[^/]+)/preferences/$',
         membershipprefs_detail, name='membershipprefs-detail'),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^api/emails/(?P<pk>[^/]+)/preferences/$', emailprefs_detail, name='emailprefs-detail'),
 )
