98d07c314ccf1820229d461aa20cdd391ac132c9
Manish Gill <gill.manish90@gmail.com>
2013-09-16 21:19:59 +0530

    Permission removed - wasn't being used

--- website/public_rest/permissions.py
+++ website/public_rest/permissions.py
@@ -7,26 +7,6 @@ from rest_framework import permissions
 
 logger = logging.getLogger(__name__)
 
-class IsValidMembershipPermission(permissions.BasePermission):
-
-    def has_object_permission(self, request, view, obj):
-        """
-        For a given user, permission should be given if
-        the user has a membership that allows her access to
-        the objects related to it (lists, memberships, etc)
-        """
-        # Jane has 2 memberships, one as a moderator and another as
-        # a member. How to ensure that she would have permissions
-        # for the first and not the second?
-        user = request.user
-        logger.debug("Incoming obj: {0}".format(obj))
-        logger.debug("Incoming user: {0}".format(user))
-        memberships = Membership.objects.filter(user=user)
-        if memberships and memberships.exists():
-            return True
-        return False
-
-
 class BaseMembershipPermission(permissions.BasePermission):
     """
     Authentication is necessary.
